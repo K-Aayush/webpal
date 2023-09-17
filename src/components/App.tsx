496c83519c77831dd15da3a6a5482b71adc74122
Aayush <ayushkarkee6@gmail.com>
2023-09-17 16:45:18 +0545

    app development component animated

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,32 +1,51 @@
+"use client"
+
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import styles, { layout } from '@/app/style'
 import Link from 'next/link'
 import { Button } from '.'
+import { delayVariantsx, containerVariants, imageVariants } from '../utils/motion'
+import { motion } from 'framer-motion'
 
 const App = () => {
     return (
-        <section className={layout.section}>
+        <motion.section
+            variants={containerVariants}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.2 }}
+            className={layout.section}>
             <div className={layout.sectionInfo}>
-                <h2 className={`${styles.heading2} text-transparent bg-clip-text bg-gradient-to-r from-cyan-800 to-cyan-300`}>
+                <motion.h2
+                    variants={delayVariantsx(0.2, "left")}
+                    className={`${styles.heading2} text-transparent bg-clip-text bg-gradient-to-r from-cyan-800 to-cyan-300`}>
                     Mobile App Develpoment
-                </h2>
-                <p className={`${styles.paragraph} max-w-[480px]`}>
+                </motion.h2>
+                <motion.p
+                    variants={delayVariantsx(0.4, "left")}
+                    className={`${styles.paragraph} max-w-[480px]`}>
                     Transforming ideas into high-performance mobile apps. Our expert team offers end-to-end services, crafting iOS, Android, and cross-platform solutions that drive your business forward. Your vision, our expertise.
-                </p>
-                <Link href={'/App'}>
-                    <Button styles="mt-10" text="Learn More" />
-                </Link>
+                </motion.p>
+                <motion.div
+                    variants={delayVariantsx(0.6, "left")}
+                >
+                    <Link href={'/App'}>
+                        <Button styles="mt-10" text="Learn More" />
+                    </Link>
+                </motion.div>
             </div>
 
-            <div className={layout.sectionImg}>
+            <motion.div
+            variants={imageVariants(0.3, "right")}
+            className={layout.sectionImg}>
                 <img
                     src="/assets/mobile app logo.png"
                     alt="mobileappdevelopment"
                     className="w-[100%] h-[100%] relative z-[5]"
                 />
-            </div>
-        </section>
+            </motion.div>
+        </motion.section>
     )
 }
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
                 className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
             >
                 <motion.div
-                    variants={delayVariantsy(0.5)}
+                    variants={delayVariantsy(0.2)}
                     initial="hidden"
                     whileInView="show"
                     className="flex flex-row items-center py-[6px] px-4 rounded-[10px] mb-2 bg-gradient-to-r from-gray-200 to-gray-100 mt-20 md:mt-10 sm:mt-0"
@@ -31,7 +31,7 @@ const Hero = () => {
                     <motion.h1
                         initial="hidden"
                         whileInView="show"
-                        variants={delayVariantsy(0.5)}
+                        variants={delayVariantsy(0.4)}
                         className="flex-1 font-semibold text-[52px] text-slate-900 leading-[100px]"
 
                     >
@@ -55,7 +55,7 @@ const Hero = () => {
                 <motion.p
                     initial="hidden"
                     whileInView="show"
-                    variants={delayVariantsy(0.5)}
+                    variants={delayVariantsy(0.6)}
                     className={`${styles.paragraph} max-w-[470px] mt-5`}
 
                 >
@@ -67,7 +67,7 @@ const Hero = () => {
                 className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
                 initial="hidden"
                 whileInView="show"
-                variants={imageVariants(0.5, "right")}
+                variants={imageVariants(0.4, "right")}
                 viewport={{ once: false, amount: 0.2 }}
             >
                 <img
