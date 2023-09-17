64d805bfda242dca2bb9b5019f1f2bf884b520f9
Aayush <ayushkarkee6@gmail.com>
2023-09-17 21:24:12 +0545

    fixed navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 'use client'
 
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import styles from '@/app/style'
 import { navLinks } from '@/constants'
 import Link from 'next/link'
@@ -11,6 +11,21 @@ import { motion } from 'framer-motion'
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const menuRef = useRef(null);
+
+  const handleClickOutside = (event: any) => {
+    if (menuRef.current && !(menuRef.current as HTMLElement).contains(event.target)) {
+      setToggle(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className={`bg-gray-50 ${styles.paddingX} ${styles.flexCenter}  fixed top-0 left-0 w-full z-[50]`}>
@@ -43,7 +58,7 @@ const Navbar = () => {
             ))}
           </ul>
 
-          <div className={`sm:hidden flex flex-1 justify-end items-center`}>
+          <div className={`sm:hidden flex flex-1 justify-end items-center`} ref={menuRef}>
             <img
               src={toggle ? '/assets/close.png' : '/assets/menu.png'}
               alt={toggle ? 'Close Menu' : 'Open Menu'}
